4c8e6ed8da5963eb5731ed665d6b1445b5d338b8
heavendew <cielocalderon02@gmail.com>
2024-04-03 16:01:52 -0500

    cambio day planner

---
+++ practicas.js
@@ -0,0 +1,9 @@
+
+function sumaElementosNum (num1, num2) {
+    const resultado = num1+num2
+    return resultado
+}
+
+const resultadoDeSuma =  sumaElementosNum (4,5)
+
+console.log(resultadoDeSuma)
